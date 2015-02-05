492698365b11a65d34129249a95362675821b551
zubiri <irecaldema14dw@ikzubirimanteo.com>
2015-02-05 07:29:02 +0000

    mejorando

--- src/Elecciones.java
+++ src/Elecciones.java
@@ -31,21 +31,21 @@ public class Elecciones {
                     System.out.println();
                     break;
                 case 3:
-                        Plaza plaza = new Plaza();
-        
-                        System.out.println("EspacioPublico");
-                        System.out.println("    Nombre: ");
-                        String nombre=sc.next();
-                        plaza.setNombre(nombre);
-                        System.out.println("    Direccion: ");
-                        String direccion=sc.next();
-                        plaza.setDireccion(direccion);
-                        System.out.println("    Capacidad: ");
-                        int capacidad=sc.nextInt();
-                        plaza.setCapacidad(capacidad);
-                        
-                        System.out.println("\nEspacioPublico");
-                        System.out.println("Nombre: "+plaza.getNombre()+" Direccion: "+plaza.getDireccion()+" Capacidad: "+plaza.getCapacidad());
+                    Plaza plaza = new Plaza();
+    
+                    System.out.println("EspacioPublico");
+                    System.out.println("    Nombre: ");
+                    String nombre=sc.next();
+                    plaza.setNombre(nombre);
+                    System.out.println("    Direccion: ");
+                    String direccion=sc.next();
+                    plaza.setDireccion(direccion);
+                    System.out.println("    Capacidad: ");
+                    int capacidad=sc.nextInt();
+                    plaza.setCapacidad(capacidad);
+                    
+                    System.out.println("\nEspacioPublico");
+                    System.out.println("Nombre: "+plaza.getNombre()+" Direccion: "+plaza.getDireccion()+" Capacidad: "+plaza.getCapacidad());
             }
         }while (opcion!=0); 
         System.out.println("\n\nEGUN ONA IZAN!!\n");
