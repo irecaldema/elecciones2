673326b9dd408bcc4ccbfa5e67a9288045f42aa7
zubiri <irecaldema14dw@ikzubirimanteo.com>
2015-02-05 07:31:15 +0000

    mejorando

--- src/Elecciones.java
+++ src/Elecciones.java
@@ -1,9 +1,5 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.io.IOException;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.util.StringTokenizer;
 public class Elecciones {
     public static void main (String args[]) throws IOException 	{
         
